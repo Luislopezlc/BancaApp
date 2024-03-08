5770a585344772d712bc99612e24d6757ad56a2b
jocsan <Jocsan@gmail.com>
2024-03-07 19:59:12 -0500

    Añadi la vista index

---
+++ lib/core/presentation/PantallaDeTransferencias/index.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+class Index extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Container(
+      height: 1800,
+      width:3000,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextButton(onPressed: () {},
+          child: Text("Editar"))
+        
+      ]),
+      );
+  }
+}
