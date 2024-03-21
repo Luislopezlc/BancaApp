1742f1f0b47723d2fd2a1e7f764ae01b52c76b64
Luis Lopez <croopluis@gmail.com>
2024-03-21 12:29:24 -0500

    add changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter_application_1/core/presentation/views/login/login.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 
-
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Future.delayed(const Duration(seconds: 10));
-  FlutterNativeSplash.remove();
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -70,7 +68,26 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+ @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
 
+  void initialization() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    // ignore_for_file: avoid_print
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
